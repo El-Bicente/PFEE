7c0cf3cef8359ec136341f2bf5cf868e710d3012
othman.elbaz <othman.elbaz@epita.fr>
2022-05-25 19:21:25 +0000

    adding lines parsing

--- format/csv_to_vtp.py
+++ format/csv_to_vtp.py
@@ -13,16 +13,27 @@ def init_cell(x):
 
 def init_faces(vtk_cells, faces):
     faces.apply(lambda x: vtk_cells.InsertNextCell(init_cell(x)), axis = 1)
+    
+def build_line(points):
+    line = vtk.vtkLine()
+    line.GetPointIds().SetId(0, points[0])
+    line.GetPointIds().SetId(1, points[1])
+    return line
+
+def init_lines(vtk_cells, lines):
+    lines.apply(lambda x: vtk_cells.InsertNextCell(build_line(x)), axis = 1)
 
 def main():
-    faces = pd.read_csv("faces.csv")
+    faces = pd.read_csv("faces_split.csv")
     points = pd.read_csv("points.csv")
+    lines = pd.read_csv("lines.csv")
     
     vtk_poly = vtk.vtkPolyData()
     vtk_pts = vtk.vtkPoints()
     vtk_cells = vtk.vtkCellArray()
 
     init_points(vtk_pts, points)
+    init_lines(vtk_cells, lines)
     init_faces(vtk_cells, faces)
 
     vtk_poly.SetPoints(vtk_pts)
