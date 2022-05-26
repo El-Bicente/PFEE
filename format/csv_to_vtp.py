df45da5baf3f292fc0d1dc4e82053a97c652f9bd
othman.elbaz <othman.elbaz@epita.fr>
2022-05-26 21:42:51 +0000

    Vectors field added

--- format/csv_to_vtp.py
+++ format/csv_to_vtp.py
@@ -4,30 +4,36 @@ import pandas as pd
 def init_points(vtk_pts, points):
     points.apply(lambda x: vtk_pts.InsertPoint(int(x[0]), x[1], x[2], x[3]), axis = 1)
 
-def init_cell(x):
-    cell = vtk.vtkTriangle()
-    Ids = cell.GetPointIds()
-    for kId in range(len(x)):
-        Ids.SetId(kId,int(x[kId]))
-    return cell
-
-def init_faces(vtk_cells, faces):
-    faces.apply(lambda x: vtk_cells.InsertNextCell(init_cell(x)), axis = 1)
-
 def build_line(points):
     line = vtk.vtkLine()
-    line.GetPointIds().SetId(0, points[0])
-    line.GetPointIds().SetId(1, points[1])
+    line.GetPointIds().SetId(0, int(points[0]))
+    line.GetPointIds().SetId(1, int(points[1]))
     return line
 
 def init_lines(vtk_cells, lines):
     lines.apply(lambda x: vtk_cells.InsertNextCell(build_line(x)), axis = 1)
 
-def main():
-    faces = pd.read_csv("faces_split_ex2.csv")
-    points = pd.read_csv("points_ex2.csv")
-    lines = pd.read_csv("lines_ex2.csv")
-    
+def build_cell(x):
+    cell = vtk.vtkTriangle()
+    Ids = cell.GetPointIds()
+    Ids.SetId(0,int(x[0]))
+    Ids.SetId(1,int(x[1]))
+    Ids.SetId(2,int(x[2]))
+    return cell
+
+def init_faces(vtk_cells, faces):
+    faces.apply(lambda x: vtk_cells.InsertNextCell(build_cell(x)), axis = 1)
+
+def build_vectors(vtk_poly_glyph, vectors_dir):
+    vectors = vtk.vtkDoubleArray()
+    vectors.SetName("Vector Field")
+    vectors.SetNumberOfComponents(3)
+    vectors.SetNumberOfTuples(vtk_poly_glyph.GetNumberOfPoints())
+    vectors_dir.apply(lambda x: vectors.SetTuple3(x[0],x[1], x[2], x[3]), axis = 1)
+    vtk_poly_glyph.GetPointData().AddArray(vectors)
+    vtk_poly_glyph.GetPointData().SetActiveVectors("Vector Field")
+
+def build_mesh(faces, points, lines):
     vtk_poly = vtk.vtkPolyData()
 
     vtk_pts = vtk.vtkPoints()
@@ -42,51 +48,44 @@ def main():
     vtk_poly.SetPolys(vtk_cells)
     vtk_poly.SetLines(vtk_lines)
 
-    vectors = vtk.vtkDoubleArray()
-    vectors.SetNumberOfTuples(5)
-    vectors.SetName("Vector Field")
-    vectors.SetNumberOfComponents(3)
-    vectors.SetTuple3(0,1,1,10)
-
-    vtk_poly.GetPointData().AddArray(vectors)
-    vtk_poly.GetPointData().SetActiveVectors("Vector Field")
-
-    arrow_source = vtk.vtkArrowSource()
-
-    add_arrows = vtk.vtkGlyph3D()
-    add_arrows.SetInputData(vtk_poly)
-    add_arrows.SetSourceConnection(arrow_source.GetOutputPort())
-    add_arrows.Update()
-
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(add_arrows.GetOutputPort())
-    """
-    array = vtk.vtkDoubleArray()
-    array.SetNumberOfComponents(3)
-    array.SetName("Three Components")
-    array.SetNumberOfTuples(300)
-    for i in range(300):
-        array.SetTuple3(i,1,1,10)
-
-    vtk_poly.GetPointData().SetVectors(array)
-    vtk_poly.GetPointData().SetActiveVectors("Three Components")
-
-    arrow = vtk.vtkArrowSource()
-    arrow.Update()
-
-    glyph = vtk.vtkGlyph3D()
-    arrowSize=0.02
-    glyph.SetScaleFactor(arrowSize)
-    glyph.SetSourceData(arrow.GetOutput())
-    glyph.SetInputData(vtk_poly)
-    glyph.SetVectorModeToUseNormal()
-    glyph.Update()
-    """
     writer = vtk.vtkXMLPolyDataWriter()
     writer.SetFileName('output.vtp')
     writer.SetInputData(vtk_poly)
 
+    writer.Write()
+
+def build_glyph(vectors_pts, vectors_dir):
+    vtk_poly_glyph = vtk.vtkPolyData()
+    vtk_vec_pts = vtk.vtkPoints()
+
+    init_points(vtk_vec_pts, vectors_pts)
+    vtk_poly_glyph.SetPoints(vtk_vec_pts)
+
+    build_vectors(vtk_poly_glyph, vectors_dir)
+    arrow_source = vtk.vtkArrowSource()
+
+    add_arrows = vtk.vtkGlyph3D()
+    add_arrows.SetInputData(vtk_poly_glyph)
+    add_arrows.SetSourceConnection(arrow_source.GetOutputPort())
+    add_arrows.Update()
+
+    writer = vtk.vtkXMLPolyDataWriter()
+    writer.SetInputConnection(add_arrows.GetOutputPort())
+    writer.SetFileName('output2.vtp')
+    writer.SetInputData(vtk_poly_glyph)
 
     writer.Write()
+
+def main():
+    faces = pd.read_csv("faces_split_ex2.csv")
+    points = pd.read_csv("points_ex2.csv")
+    lines = pd.read_csv("lines_ex2.csv")
+    
+    vectors_pts = pd.read_csv("vectors.csv")
+    vectors_dir = pd.read_csv("vectors_dir.csv")
+    
+    build_mesh(faces, points, lines)
+    build_glyph(vectors_pts, vectors_dir)
+
 if __name__ == "__main__":
     main()
