306b7b16cb6b29b0732cf7cc5f936a5883dc3e6d
william.guillet <william.guillet@epita.fr>
2022-10-13 23:55:29 +0200

    add the level of faces

--- function_to_csv/three_dim_function_to_csv2.py
+++ function_to_csv/three_dim_function_to_csv2.py
@@ -50,16 +50,52 @@ def get_local_edges(ids: dict, pos: Coordinates, reversed: bool):
 
     return local_edges
 
+def get_local_faces(ids: dict, pos: Coordinates, reversed: bool):
+    if not reversed:
+        local_faces = [
+            # right face
+            (ids[pos.z, pos.y, pos.x], ids[pos.z, pos.y, pos.x + 1], ids[pos.z + 1, pos.y, pos.x]),
+            (ids[pos.z, pos.y, pos.x + 1], ids[pos.z + 1, pos.y, pos.x], ids[pos.z + 1, pos.y, pos.x + 1]),
+
+            # back face
+            (ids[pos.z, pos.y, pos.x], ids[pos.z, pos.y + 1, pos.x], ids[pos.z + 1, pos.y, pos.x]),
+            (ids[pos.z, pos.y + 1, pos.x], ids[pos.z + 1, pos.y, pos.x], ids[pos.z + 1, pos.y + 1, pos.x]),
+
+            # botton face
+            (ids[pos.z, pos.y, pos.x], ids[pos.z, pos.y, pos.x + 1], ids[pos.z, pos.y + 1, pos.x]),
+            (ids[pos.z, pos.y, pos.x + 1], ids[pos.z, pos.y + 1, pos.x], ids[pos.z, pos.y + 1, pos.x + 1])
+        ]
+    else:
+        local_faces = [
+            # right face
+            (ids[pos.z, pos.y, pos.x], ids[pos.z + 1, pos.y, pos.x], ids[pos.z + 1, pos.y, pos.x + 1]),
+            (ids[pos.z, pos.y, pos.x], ids[pos.z, pos.y, pos.x + 1], ids[pos.z + 1, pos.y, pos.x + 1]),
+
+            # back face
+            (ids[pos.z, pos.y, pos.x], ids[pos.z + 1, pos.y, pos.x], ids[pos.z, pos.y + 1, pos.x]),
+            (ids[pos.z, pos.y + 1, pos.x], ids[pos.z, pos.y + 1, pos.x], ids[pos.z + 1, pos.y + 1, pos.x]),
+
+            # botton face
+            (ids[pos.z, pos.y, pos.x], ids[pos.z, pos.y, pos.x + 1], ids[pos.z, pos.y + 1, pos.x + 1]),
+            (ids[pos.z, pos.y, pos.x], ids[pos.z, pos.y + 1, pos.x], ids[pos.z, pos.y + 1, pos.x + 1])
+        ]
+
+    return local_faces
+
 def build_volume(ids: np.array):
     reversed = False
     edges = []
+    faces = []
     for z in range(ids.shape[0] - 1):
         for y in range(ids.shape[1] - 1):
             reversed = not reversed
             for x in range(ids.shape[2] - 1):
-                edges += get_local_edges(ids, Coordinates((x, y, z)), reversed)
+                current_pos = Coordinates((x, y, z))
+                edges += get_local_edges(ids, current_pos, reversed)
+                faces += get_local_faces(ids, current_pos, reversed)
 
     print(f"number of edges: {len(edges)}")
+    print(f"number of faces: {len(faces)}")
 
                 
 
